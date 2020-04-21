f3f6a1076e964a179e02d49b0991173107cbbb8b
Gabriel <creeplays@gmail.com>
2020-04-20 23:30:29 -0300

    Correção do server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/CRUD-Frontend'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/CRUD-Frontend/index.html'));
 });
