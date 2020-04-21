e11c7c826a7780ff5938b85c814fd3271c0ed436
Gabriel <creeplays@gmail.com>
2020-04-20 23:56:54 -0300

    Correção dos scripts

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/crud-frontend'));
 
-app.get('/', function(req,res) {
+app.get('/*', function(req,res) {
     
 res.sendFile('index.html', {root: 'dist/crud-frontend/'});
 
